7358e82e461fa61e90853c831de357e8cb33128b
Garvita Asthana <garvitasthana11@gmail.com>
2023-09-12 20:23:41 +0530

    first commit

---
+++ redux toolkit/index.cjs
@@ -0,0 +1,22 @@
+const store = require('./app/store.cjs')
+const cakeActions = require('./features/cake/cakeSlice.cjs').cakeActions
+const icecreamActions = require('./features/icecream/icecreamSlice.cjs').icecreamActions
+const fetchUsers = require('./features/user/userSlice.cjs').fetchUsers
+
+console.log('Initial State: ', store.getState());
+const unsubscribe = store.subscribe(() => {
+    console.log('Updated State: ', store.getState())
+})
+
+store.dispatch(fetchUsers());
+
+// store.dispatch(cakeActions.ordered())
+// store.dispatch(cakeActions.ordered())
+// store.dispatch(cakeActions.ordered())
+// store.dispatch(cakeActions.restocked(3))
+
+// store.dispatch(icecreamActions.ordered())
+// store.dispatch(icecreamActions.ordered())
+// store.dispatch(icecreamActions.restocked(2))
+
+// unsubscribe();
